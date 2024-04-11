eff50dd60139b4af7c8e2404cd46245a5d991eac
sjovy <thomas.sjovy@gmail.com>
2024-04-11 07:27:26 +0200

    started setting up tests

--- src/test/java/se/lexicon/data/impl/ParkingSpotDaoImplTest.java
+++ src/test/java/se/lexicon/data/impl/ParkingSpotDaoImplTest.java
@@ -1,6 +1,24 @@
 package se.lexicon.data.impl;
 
+import se.lexicon.model.ParkingSpot;
+
+import java.util.*;
+
 public class ParkingSpotDaoImplTest {
     //todo: Implement required JUnit tests
+
+    /*ParkingSpot create(ParkingSpot parkingSpot);
+
+    Optional<ParkingSpot> find(int spotNumber);
+
+    boolean remove(int spotNumber);
+
+    List<ParkingSpot> findAll();
+
+    List<ParkingSpot> findByAreaCode(int areaCode);
+
+    void occupyParkingSpot(int spotNumber);
+
+    void vacateParkingSpot(int spotNumber);*/
 
 }
--- src/test/java/se/lexicon/data/impl/ReservationDaoImplTest.java
+++ src/test/java/se/lexicon/data/impl/ReservationDaoImplTest.java
@@ -1,6 +1,22 @@
 package se.lexicon.data.impl;
 
+import se.lexicon.model.Reservation;
+
+import java.util.Optional;
+
 public class ReservationDaoImplTest {
     //todo: Implement required JUnit tests
+
+    /*Reservation create(Reservation reservation);
+
+    Optional<Reservation> find(String id);
+
+    boolean remove(String id);
+
+    Reservation findByCustomerId(int customerId);
+
+    Reservation findByVehicleLicensePlate(String licensePlate);
+
+    Reservation findByParkingSpotNumber(int spotNumber);*/
 
 }
